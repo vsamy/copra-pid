38887b2c7a6304b9a340fc969dd2092d7cb16c4e
Vincent Samy <vincent.samy@lirmm.fr>
2017-10-19 16:47:55 +0200

    license

--- src/copra/solverUtils.cpp
+++ src/copra/solverUtils.cpp
@@ -1,4 +1,21 @@
-
+/*      File: solverUtils.cpp
+*       This file is part of the program copra
+*       Program description : This a C++ implementation of a Time Invariant Linear Model Predictive Controller (LMPC) done in C++14 with python bindings
+*       Copyright (C) 2017 -  Vincent Samy (LIRMM). All Right reserved.
+*
+*       This software is free software: you can redistribute it and/or modify
+*       it under the terms of the CeCILL-C license as published by
+*       the CEA CNRS INRIA, either version 1
+*       of the License, or (at your option) any later version.
+*       This software is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*       CeCILL-C License for more details.
+*
+*       You should have received a copy of the CeCILL-C License
+*       along with this software. If not, it can be found on the official website
+*       of the CeCILL licenses family (http://www.cecill.info/index.en.html).
+*/
 #include <copra/solverUtils.h>
 #ifdef EIGEN_QUADPROG
 #include <copra/QuadProgSolver.h>
